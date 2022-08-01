0e4f4741e261eeebc616d86c895d98400aecb108
Aleksandr Belozerov <allbel900@gmail.com>
2022-08-01 13:31:58 +0300

    Edit HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
 import './Header.css'
+import {PATH} from "./Pages";
 
 function Header() {
     return (
         <div className={'boxLink'}>
-            <NavLink to={'/pre-junior'}>pre-junior</NavLink>
-            <NavLink to={'/junior'}>junior</NavLink>
-            <NavLink to={'/junior+'}>junior+</NavLink>
+            <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR}>junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>junior+</NavLink>
         </div>
     )
 }

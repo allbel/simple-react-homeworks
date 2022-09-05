705c471682dfadfa6ab997bf84914ff5bdd62a4a
Aleksandr Belozerov <allbel900@gmail.com>
2022-09-05 21:37:25 +0300

    Home work 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,17 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './common/HW11.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+
+    const onChangeRange = (value: [number, number]) => {
+        const [value1, value2] = value
+        setValue1(value1)
+        setValue2(value2)
+    }
 
     return (
         <div>
@@ -13,18 +20,27 @@ function HW11() {
 
             {/*should work (должно работать)*/}
             <div>
-                <span>{value1}</span>
+                <span className={s.value1Box}>{value1}</span>
                 <SuperRange
                     // сделать так чтоб value1 изменялось
+                    value={value1}
+                    onChangeRange={setValue1}
                 />
             </div>
 
             <div>
-                <span>{value1}</span>
+                <span className={s.value1Box}>{value1}</span>
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
+                    value={[value1, value2]}
+                    onChangeRange={onChangeRange}
+                    // min={10}
+                    // max={90}
+                    // step={10}
+                    // marks={true}
+                    // disable={true}
                 />
-                <span>{value2}</span>
+                <span className={s.value2Box}>{value2}</span>
             </div>
 
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
+import {Box, Slider} from "@mui/material";
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
     value?: [number, number]
     // min, max, step, disable, ...
+    min?: number
+    max?: number
+    step?: number
+    marks?: boolean
+    disable?: boolean
+
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
+        min, max, step,
+        marks, disable
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+
+    const onChangeCallback = (event: Event, newValue: number | number[]) => {
+        onChangeRange && onChangeRange(newValue as [number, number])
+    }
 
     return (
         <>
-            DoubleRange
+            <Box sx={{display: 'inline-block', width: 130 }}>
+                <Slider
+                    value={value}
+                    onChange={onChangeCallback}
+                    valueLabelDisplay="auto"
+                    disableSwap
+                    min={min}
+                    max={max}
+                    step={step}
+                    marks={marks}
+                    disabled={disable}
+                />
+            </Box>
         </>
     )
 }
